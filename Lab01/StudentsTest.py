7c88c5fe1866a635dcd52c0c9baa9a50320abf1f
lyc <a0926858615@gmail.com>
2023-03-09 14:40:48 +0800

    test coverage 100%

--- Lab01/StudentsTest.py
+++ Lab01/StudentsTest.py
@@ -7,12 +7,31 @@ class Test(unittest.TestCase):
     user_name = ['John', 'Mary','Thomas','Jane']
     user_id = []
 
+
     # test case function to check the Students.set_name function
     def test_0_set_name(self):
         #TODO
+        print("Start set_name test")
+        for i in range(len(self.user_name)):
+            self.user_id.append(self.students.set_name(self.user_name[i]))
+            print(self.user_id[i], self.user_name[i])
+            for j in range(len(self.user_id)-1):
+                self.assertNotEqual(self.user_id[i], self.user_id[j])
         pass
+        print("\nFinish set_name test\n\n")
+
 
     # test case function to check the Students.get_name function
     def test_1_get_name(self):
         #TODO
+        print("Start get_name test\n")
+        print("user_id length =", len(self.user_id))
+        print("user_name length =", len(self.user_name), "\n")
+        for i in range(len(self.user_name)+1):
+            name = self.students.get_name(i)
+            print("id", i, ":", name)
+            if i<len(self.user_name):
+                self.assertIs(name, self.user_name[self.user_id[i]])
+            else:
+                self.assertNotIn(name, self.students.name)
         pass
